0a1e917483db22c4c49843564cbf84f2c2e9bda4
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-08-07 14:23:27 +0500

    raihaan assigment

---
+++ raihan assigment.js
@@ -0,0 +1,36 @@
+var a = (Number (prompt("What is your number")));
+if (a == 1){
+
+    document.write("One");
+    
+}
+if (a == 2){
+    document.write("Two");
+}
+if (a == 3){
+    document.write("Three");
+}
+if (a == 4){
+    document.write("Four");
+}
+if (a == 5){
+    document.write("Five");
+}
+if (a == 6){
+
+    document.write("Six");
+    
+}
+if (a == 7){
+    document.write("Seven");
+}
+if (a == 8){
+    document.write("Eight");
+}
+if ( a == 9){
+    document.write("Nine");
+
+}
+if (a == 10){
+    document.write("Ten");
+}
