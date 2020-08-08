399ba37515a1a9dec717a592006e1f8ea28b161c
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-08-08 20:32:30 +0500

    Task11 completed

---  assigment1/task11.js
+++  assigment1/task11.js
@@ -1,6 +1,6 @@
-var celcius = 25;
-var farhentit = 70;
-var result = (farhentit - 32) * 5 / 9;
-document.write(result);
-result = (celcius * 9 / 5) + 32;
-document.write(result);
+var celcius = 105;
+var convertInToFarhenhiet = (celcius * 9 / 5) + 32;
+document.write(celcius + " <sup> o </sup> C is " + convertInToFarhenhiet + "<sup> o </sup> F");
+var farhenhiet = 70;
+var converInToCelcius = (farhenhiet - 32) * 5 / 9;
+document.write("</br>" +farhenhiet + " <sup> o </sup> F is " + converInToCelcius + " <sup> o </sup> C ");
