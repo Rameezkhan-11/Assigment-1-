280f9e29354fce3c78b76487fcfa77e460e5f637
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-08-08 17:26:53 +0500

    Changed

---  assigment1/task11.js
+++  assigment1/task11.js
@@ -1,5 +1,6 @@
-var celcius = (77 -32) * 5 / 9;
-var farhentit = (celcius * 9 / 5) + 32;
-console.log(farhentit);
-
-console.log(celcius);
+var celcius = 25;
+var farhentit = 70;
+var result = (farhentit - 32) * 5 / 9;
+document.write(result);
+result = (celcius * 9 / 5) + 32;
+document.write(result);
