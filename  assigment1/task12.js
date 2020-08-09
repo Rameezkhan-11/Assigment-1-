ee12c5c04e5c5f34c7da8c473f43fd3ab923b0af
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-08-09 14:05:20 +0500

    Task completed

---
+++  assigment1/task12.js
@@ -0,0 +1,4 @@
+var usDollar = 10;
+var saudiRiyals = 25;
+var convert = (usDollar * 155 + saudiRiyals * 41);
+document.write("<h1> Currency In PKR</h1> <br> <br> Total Currency in PKR is: " + convert );
---
+++  assigment1/task13.js
@@ -0,0 +1,16 @@
+var a = 10;
+document.write("<h1> Result:</h1>\n The value of a is: " + a);
+document.write("<br>_ _ _ _ _ _ _ _ _  _ _ _ _ _ _ _ _ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ <br>")
+++a;
+document.write("<br> The value of ++a is: " + a);
+document.write("<br> Now the value of a is: " + a);
+var b = a++;
+document.write("<br>The value of a++ is: " + b);
+document.write("<br> Now the value of a is:  " + a);
+--a;
+document.write("<br> The value of --a is: " + a);
+document.write("<br> Now the value of a is: " + a);
+b = a--;
+document.write("<br> The value of a-- is: " + b);
+document.write("<br> Now the value of a is: " + a);
+
