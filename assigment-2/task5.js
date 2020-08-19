f5d176d820deb5cfe1ab9ff6419fbe9859c9c7fc
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-08-19 13:02:11 +0500

    Task-4 done

---
+++ assigment-2/task5.js
@@ -0,0 +1,12 @@
+
+var guessGame =(Number (prompt("What is secret Number")));
+
+var secretNumber = 4;
+if (guessGame === secretNumber){
+    alert("Bingo! Correct Answer");
+
+}
+else if (guessGame += secretNumber){
+    alert("Close enough to the correct answer");
+
+}
