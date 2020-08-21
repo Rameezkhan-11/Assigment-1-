3e8ba133ae3aaf62ec25fd8c3969e4a42feafab0
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-08-21 17:01:28 +0500

    Updated task10.js

--- assigment-2/task10.js
+++ assigment-2/task10.js
@@ -1,13 +1,16 @@
 var password = 1234;
 var userInput = prompt("Please enter yout password");
-
-if ( userInput == password ){
+var usI = prompt("Reconfrim password");
+if ( userInput == password && usI == password){
   alert("Correct the passwords you enters matches original password");
 }
-else{
+else if (userInput != password){
   document.write("Incorrect password: " + userInput);
+  
 }
-
+else if (usI != password){
+  document.write("Reconfirm password incorrect " + usI);
+}
 
 
 
