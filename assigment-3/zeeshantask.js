3080ae789621e723879f59e3d6ad6f10c17923a4
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-08-22 21:35:09 +0500

    Updated file names

---
+++ assigment-3/zeeshantask.js
@@ -0,0 +1,23 @@
+for (var i = 1; i <=7; i++){
+    if(i == 1){
+ 
+       document.write("*");}
+       else if (i ==2){
+           document.write("<br>**");
+       }
+    else if ( i == 3){
+        document.write("<br>***");
+    }
+else if (i == 4){
+    document.write("<br> ****");
+}
+else if (i == 5){
+    document.write("<br>*****");
+}
+else if (i == 6){
+    document.write("<br>******");
+}
+else if (i == 7){
+    document.write("<br> *******");
+}
+}
