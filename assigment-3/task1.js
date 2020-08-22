958ba76b6a2bfe1703d3d2d4b03608a8c6c9ccf4
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-08-22 23:25:03 +0500

    Task1 completed

---
+++ assigment-3/task1.js
@@ -0,0 +1,14 @@
+var qualification = ["SSC","HSC","BCS", "BS", "BCOM", "MS","M. Phill.","PhD"];
+        document.write("<h1> Qualification: </h1>")
+        document.write("<h3> 1 ) " + qualification[0] + "</h3>");
+        document.write("<h3> 2 ) " + qualification[1] + "</h3>");
+        document.write("<h3> 3 ) " + qualification[2] + "</h3>");
+        document.write("<h3> 4 ) " + qualification[3] + "</h3>");
+        document.write("<h3> 5 ) " + qualification[4] + "</h3>");
+        document.write("<h3> 6 ) " + qualification[5] + "</h3>");
+        document.write("<h3> 7 ) "  + qualification[6] + "</h3>");
+        document.write("<h3> 8 ) "  + qualification[7] + "</h3>");
+        
+        
+
+
