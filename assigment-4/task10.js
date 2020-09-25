335549807602b803bcd989cf0dcce5b3a7bae39a
rameezkhan-hub <rameezkhansadozai@gmail.com>
2020-09-25 23:49:46 +0500

    modified:

---
+++ assigment-4/task10.js
@@ -0,0 +1,17 @@
+var userInput = (Number (prompt("Write a Number")));
+
+
+
+console.log(typeof (userInput));
+
+if(userInput > 0){
+
+console.log(userInput.toFixed(2));
+
+
+}
+else{
+
+console.log(userInput.toFixed(4));
+
+}
