d2753ab752a92de8de4443851e6ee6ea4c1a8fd0
rameez <rameezkhansadozai@gmail.com>
2020-11-10 17:52:24 +0500

    New assigment some updated files

---
+++ assigment-5/task10.js
@@ -0,0 +1,26 @@
+var userInput = prompt("Write a text i will tell you the counts of vowels");
+var vowels = "aeiou";
+
+function vow(){
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vow();
---
+++ assigment-5/task5.js
@@ -0,0 +1,14 @@
+var userInput = prompt("Withdrawl amount");
+function wt (){
+
+
+   console.log(userInput[0]);
+   console.log(Math.round(userInput[1] / 100));
+
+
+
+
+
+
+}
+wt();
